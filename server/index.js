0af937015d7bb8bdd6d730a1a2373d1a73229e62
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-10-22 12:25:46 -0300

    criptografia

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import Express from "express";
 import { criarTabelas, User } from "./db.js";
+import bcryptjs from "bcrypt"
 
 const app = Express()
 app.use(Express.json())
@@ -11,15 +12,19 @@ app.post('/registro', async (req, res) =>{
         res.send('você deve preencher todos os campos')
         return
     }
-const userExiste = await User.findAll({email: email})
+const userExiste = await User.findOne({ where: {email: email} })
 if(userExiste){
     res.send('O usuário já existe')
     return
 }
-
-    const teste = await User.create({nome, sobrenome, email, senha, dataNascimento})
+    const senhaCriptografada = bcryptjs.hashSync(senha, 10)
+    const teste = await User.create({nome, sobrenome, email, senha: senhaCriptografada, dataNascimento})
     res.send('Usuário Criado')
+
+    
 })
+
+
 
 app.post('/login', (req, res) =>{
     const{ email, senha } = req.body
